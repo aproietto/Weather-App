57a69f1085ddfa95831adffa6262917c3ac63180
Ashley Proiett <minolta028@aol.com>
2025-07-24 13:45:38 -0700

    routes.rb - routing to zipcode page
    routes.rb - routing to zipcode
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    -- INSERT --routes.rb - routing to zipcode page
    
    routes.rb - routing to zipcode

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,24 +21,27 @@ class HomeController < ApplicationController
       @api_color = "silver"
     elsif @final_output <= 50 
       @api_color = "green"
-      @api_description = ""
+      @api_description = "Good (0-50) Enjoy your outdoor activities"
     elsif @final_output >= 51 && @final_output <= 100
       @api_color = "yellow"
-      @api_description = ""
+      @api_description = "Moderate (51-100) If you are unusually sensitive to ozone, consider reducing your activity level or shorten the amount of time you are active outdoors."
     elsif @final_output >= 101 && @final_output <= 150
       @api_color = "orange"
-      @api_description = ""
+      @api_description = "(101-150) Unhealthy for Sensitive Groups"
     elsif @final_output >= 151 && @final_output <= 200
       @api_color = "red"
-      @api_description = ""
+      @api_description = "(151-200) Unhealthy"
     elsif @final_output >= 201 && @final_output <= 300
       @api_color = "purple"
-      @api_description = ""
+      @api_description = "(201-300) Very Unhealthy"
     elsif @final_output >= 301 && @final_output <= 500
       @api_color = "maroon"
-      @api_description = ""
+      @api_description = "(301+) Hazardous"
 
     end
 
+  end
+
+  def zipcode
   end
 end
